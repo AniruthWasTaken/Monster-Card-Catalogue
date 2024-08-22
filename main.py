81cdac886bb240d1ab457e095c11337766a7cf1d
AniruthWasTaken <142752969+AniruthWasTaken@users.noreply.github.com>
2024-08-22 01:13:43 +0000

    Imported easygui and crated the main program loop

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # This script manages the main player input: Add, Remove, Search/Edit, Quit
 # This will be done in a while True loop. It breaks only when the user chooses to quit.
+
+import easygui as eg
+
+while True:
+  user_input = eg.buttonbox("What would you like to do?",
+                            choices=["Add", "Remove", "Search/Edit", "Quit"])
+  if user_input == "Add":
+    eg.msgbox("Taylor")
+  elif user_input == "Remove":
+    eg.msgbox("Cody")
+  elif user_input == "Search/Edit":
+    eg.msgbox("Luke")
+  elif user_input == "Quit":
+    eg.msgbox("Thank you for using the Monster Catalogue!")
+    break
